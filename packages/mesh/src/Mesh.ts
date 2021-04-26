136e159de2c19084763be0c0436ccd11ff94a1e0
dev7355608 <dev7355608@gmail.com>
2021-04-27 00:03:06 +0200

    Update roundPixels documentation

--- packages/mesh/src/Mesh.ts
+++ packages/mesh/src/Mesh.ts
@@ -217,7 +217,7 @@ export class Mesh<T extends Shader = MeshMaterial> extends Container
     }
 
     /**
-     * If true PixiJS will Math.floor() x/y values when rendering, stopping pixel interpolation.
+     * If true PixiJS will Math.round() x/y values when rendering, stopping pixel interpolation.
      * Advantages can include sharper image quality (like text) and faster rendering on canvas.
      * The main disadvantage is movement of objects may appear less smooth.
      * To set the global default, change {@link PIXI.settings.ROUND_PIXELS}
--- packages/sprite/src/Sprite.ts
+++ packages/sprite/src/Sprite.ts
@@ -561,7 +561,7 @@ export class Sprite extends Container
     }
 
     /**
-     * If true PixiJS will Math.floor() x/y values when rendering, stopping pixel interpolation.
+     * If true PixiJS will Math.round() x/y values when rendering, stopping pixel interpolation.
      * Advantages can include sharper image quality (like text) and faster rendering on canvas.
      * The main disadvantage is movement of objects may appear less smooth.
      * To set the global default, change {@link PIXI.settings.ROUND_PIXELS}
